ca5c8c6c9b94f26c7a9729aa37f5eabdd862caf1
Vasiliy Shubko <flit77@gmail.com>
2017-08-17 22:41:16 +0200

    routes added fix

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Layout from 'layouts/Main';
+import Layout from '../layouts/Main';
 import { getPosts } from 'api/posts';
-import { Link } from 'routes';
+import { Link } from '../routes';
 
 import Post from 'components/Post';
 
